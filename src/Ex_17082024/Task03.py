379f77941fc7182c9da4199293733791de322f31
PrashantTurate <turate.prashant@gmail.com>
2024-08-19 15:31:57 +0530

    Task03 updated

---
+++ src/Ex_17082024/Task03.py
@@ -0,0 +1,20 @@
+
+#Difference between the = operator and the == operator in Python
+
+a = 25
+b = 28
+print(a,b)
+print(a==b)
+
+#What does the ** operator do in Python, and how is it used?
+
+c = 5
+d = 3
+print(c*d)
+print(c**d)
+print((pow(c,d)))
+
+#What does the ^ operator do in Python
+e = 12
+f = 15
+print(e^f)
