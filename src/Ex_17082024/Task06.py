c9a515c2e7dab0a37b9acd5cdf01ce07c3a8b800
PrashantTurate <turate.prashant@gmail.com>
2024-08-19 16:02:14 +0530

    Task06 updated

--- src/Ex_17082024/Task06.py
+++ src/Ex_17082024/Task06.py
@@ -3,10 +3,6 @@
 
 number = int(input("Enter a number: "))
 Square = pow(number,2)
-Square2 = number ** 2
 Cube = pow(number,3)
-Cube2 = number ** 3
 print("Square of a number is : ", Square)
-print("Square of a number is : ", Square2)
 print("Cube of a number is : ", Cube)
-print("Cube of a number is : ", Cube2)
