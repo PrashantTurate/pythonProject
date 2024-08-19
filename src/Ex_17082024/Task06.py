3f95465709252aef5f7d5e8f0b909f8615c4a234
PrashantTurate <turate.prashant@gmail.com>
2024-08-19 15:57:26 +0530

    Task06 updated

---
+++ src/Ex_17082024/Task06.py
@@ -0,0 +1,12 @@
+
+#Develop a Python script that calculates the square and cube of a given number.
+
+number = int(input("Enter a number: "))
+Square = pow(number,2)
+Square2 = number ** 2
+Cube = pow(number,3)
+Cube2 = number ** 3
+print("Square of a number is : ", Square)
+print("Square of a number is : ", Square2)
+print("Cube of a number is : ", Cube)
+print("Cube of a number is : ", Cube2)
