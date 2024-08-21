82e3d8864f11c265ce80b8ae083c1bcd78561e1e
PrashantTurate <turate.prashant@gmail.com>
2024-08-21 20:57:07 +0530

    Task07 updated

--- src/Ex_20082024/Task07.py
+++ src/Ex_20082024/Task07.py
@@ -1,4 +1,6 @@
-# Create a program that determines whether a given year is a leap year. A leap year is divisible by 4, but not by 100 unless it is also divisible by 400
+
+'''Create a program that determines whether a given year is a leap year. A leap year is divisible by 4,
+but not by 100 unless it is also divisible by 400'''
 
 Year = int(input("Enter a year: "))
 if Year % 400 == 0 and Year % 100 == 0:
