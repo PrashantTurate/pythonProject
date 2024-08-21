0317f71dbf594de3f0743e06a9b96eccb0607569
PrashantTurate <turate.prashant@gmail.com>
2024-08-21 21:16:45 +0530

    Task08 updated

--- src/Ex_20082024/Task08.py
+++ src/Ex_20082024/Task08.py
@@ -3,3 +3,12 @@
 determine if the triangle is equilateral (all sides are equal), isosceles (exactly two sides are equal),
 or scalene (no sides are equal).'''
 
+side1 = input("Enter a side1 length: ")
+side2 = input("Enter a side2 length: ")
+side3 = input("Enter a side3 length: ")
+if side1==side2==side3:
+    print(f"Triangle is Equilateral")
+elif side1==side2 or side2==side3 or side3==side1:
+    print("Triangle is Isosceles")
+else:
+    print("Triangle is Scalene")
