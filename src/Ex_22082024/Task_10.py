bf6c933141b6a94683af9ffb15b753784b2b9a55
PrashantTurate <turate.prashant@gmail.com>
2024-08-23 17:52:43 +0530

    Task10 updated

--- src/Ex_22082024/Task_10.py
+++ src/Ex_22082024/Task_10.py
@@ -9,7 +9,7 @@ if num < 0 :
 elif num == 0:
     print("Factorial of 0 is 1")
 else:
-    for i in range(1,num+1):
+    for i in range(num,1,-1):
         fact=fact*i
     print(f"The factorial of number {num} is: {fact}")
 
