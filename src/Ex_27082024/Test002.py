a1776901681045ed4b0d905007f26723d5aca36b
PrashantTurate <turate.prashant@gmail.com>
2024-08-27 08:55:48 +0530

    Test002 updated

--- src/Ex_27082024/Test002.py
+++ src/Ex_27082024/Test002.py
@@ -1,28 +1,12 @@
-import math
 
-def sum_three(a,b,c):
-   return a+b+c
+Glb = 20
+def func():
+    a = 10
+    print(a)
 
-#p = lambda a,b,c: a+b+c
-#print(op(1,2,3))
+def func2(Glb):
+    Glb = 50
+    print(Glb)
 
-def give_me_power(num):
-    return math.pow(num,2)
-
-a=give_me_power(5)
-print(a)
-
-op = lambda num:math.pow(num,2)
-print(op(5))
-
-output = lambda:math.pow(int(input("Enter a number: ")),3)
-print(output())
-
-def find_odd_even(num):
-    if num % 2 == 0:
-        print(num, "is an odd number")
-    else:
-        print(num, "is an even number")
-
-find_odd_even(4)
-
+func()
+func2(Glb)
