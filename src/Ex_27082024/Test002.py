44f5c2c16169ed9239993fba198a32585e256f71
PrashantTurate <turate.prashant@gmail.com>
2024-08-31 00:14:00 +0530

    Test002 updated

--- src/Ex_27082024/Test002.py
+++ src/Ex_27082024/Test002.py
@@ -1,24 +1,24 @@
-'''
-Glb = 20
-def func():
-    a = 10
-    print(a)
-def func2(Glb):
-    Glb = 50
-    print(Glb)
+#
+# Glb = 20
+# def func():
+#     a = 10
+#     print(a)
+# def func2(Glb):
+#     Glb = 50
+#     print(Glb)
+#
+# func()
+# func2(Glb)
 
-func()
-func2(Glb)
 
+# def Pizza_making(*ingredients):
+#     for i in ingredients:
+#         print(i)
+# print(" program start")
+# Ajay = Pizza_making("Tomato")
+# Vijay = Pizza_making("Olives","mushroom","pineapple","sweetcorn")
+# print(" program end")
 
-def Pizza_making(*ingredients):
-    for i in ingredients:
-        print(i)
-print(" program start")
-Ajay = Pizza_making("Tomato")
-Vijay = Pizza_making("Olives","mushroom","pineapple","sweetcorn")
-print(" program end")
-'''
 
 my_shoping_list = ["bread", "milk", "butter"]
 print(my_shoping_list)
