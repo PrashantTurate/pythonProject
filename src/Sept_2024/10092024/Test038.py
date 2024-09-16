04deecc7d9e96bfe81b78d7cd9f75635df60b6a1
PrashantTurate <turate.prashant@gmail.com>
2024-09-16 22:28:35 +0530

    Test038 file updated

--- src/Sept_2024/10092024/Test038.py
+++ src/Sept_2024/10092024/Test038.py
@@ -2,3 +2,23 @@
 
 with open('TestData.txt','a') as file:
     file.write(" Hello, How are you? ")
+
+# How to read a file
+with open('TestData.txt', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        print(line,end="")
+
+'''
+try:
+    with open("TestData.txt", "r") as file:
+        content = file.readlines()
+        print(content)
+except FileNotFoundError as fnfr:
+    print(fnfr)
+finally:
+    try:
+        file.close()
+    except NameError as ne:
+        print("NE")
+'''
