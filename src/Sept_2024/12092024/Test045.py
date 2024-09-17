f32352a75481726633a71265491f482670c2f1d2
PrashantTurate <turate.prashant@gmail.com>
2024-09-17 22:27:14 +0530

    Test045 file updated

---
+++ src/Sept_2024/12092024/Test045.py
@@ -0,0 +1,20 @@
+#Ordered Dict - Maintain insertion order
+
+from collections import OrderedDict
+
+od  = OrderedDict()
+od["Apple"] = 2
+od["Banana"] = 1
+od["Pear"] = 6
+od["Grapes"] = 3
+print(od)
+
+
+d = dict()
+d["Name"] = "Prashant"
+d["Age"] = 34
+d["ID"] = 2122
+d["Address"] = "Pune"
+d["Mobile"] = 9087654532
+print(d)
+
