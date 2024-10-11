1c8ab42cac5aefff44dae21a5a650791323757df
PrashantTurate <turate.prashant@gmail.com>
2024-10-12 01:21:17 +0530

    Test051 file updated

--- src/Oct_2024/10102024/test_Test051.py
+++ src/Oct_2024/10102024/test_Test051.py
@@ -2,13 +2,30 @@
 # Similar to pre condition, post condition.
 # Pre Condition - token, booking Id - Fixture
 
-
 import pytest
 
 @pytest.fixture()
 def is_married():
     return True
 
-
 def test_confirmation(is_married):
     assert is_married == True
+
+
+@pytest.fixture()
+def create_token():
+    return "abc"
+
+@pytest.fixture()
+def create_booking_id():
+    return 123
+
+
+def test_update_req_1(create_token, create_booking_id):
+    print("Token ->", create_token)
+    print("Booking ID -> ", create_booking_id)
+
+
+def test_update_req_2(create_token, create_booking_id):
+    print("Token ->", create_token)
+    print("Booking ID -> ", create_booking_id)
